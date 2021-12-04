77696d84a19eae6b1ffd3e7d3f05f8db162418cb
Siddesh Patil <87792589+Coolasid@users.noreply.github.com>
2021-12-03 21:39:09 -0500

    3-12

--- scripts/getData.js
+++ scripts/getData.js
@@ -1,3 +1,23 @@
 //Get details of Any perticular user
 
-//1. 
+//1. userName ( argument )
+
+//2. api call to github
+
+async function getData(user){
+
+try{
+
+    let res = await fetch(`https://api.github.com/users/${user}`)
+
+    let data = await res.json();
+
+    return data;
+
+}
+catch(error){
+    console.log(error);
+}
+}
+
+export default getData;
